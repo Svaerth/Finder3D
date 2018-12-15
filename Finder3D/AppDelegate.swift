c4e2ad9ad57572c0acbcbbc0564a6ce8eea48d16
Will Anderson <svaerth@little-mac.attlocal.net>
2018-12-15 12:00:00 -0600

    Initial Commit

---
+++ Finder3D/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Finder3D
+//
+//  Created by Will Anderson on 12/15/18.
+//  Copyright © 2018 Will Anderson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
